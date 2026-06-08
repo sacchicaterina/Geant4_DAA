214e03cd816ac4992502b8caaa8c85b5bb0d54aa
Chiara Ritorto <132816772+chiararitorto@users.noreply.github.com>
2026-06-08 19:43:25 +0200

    Refactor main function and improve formatting

--- AnaEx01/AnaEx01.cc
+++ AnaEx01/AnaEx01.cc
@@ -32,10 +32,14 @@
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
+#include "G4ScoringManager.hh"
+
 #include "DetectorConstruction.hh"
 #include "ActionInitialization.hh"
 
 #include "G4RunManagerFactory.hh"
+
+#include "G4ScoringManager.hh" // stessa cosa fatta nell'esercizio sul BP
 
 #include "G4UImanager.hh"
 
@@ -47,46 +51,58 @@
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
-int main(int argc,char** argv)
+int main(int argc, char **argv)
 {
-  G4String physListName;
-  
+  //G4String physListName;
+  G4String physListName = "QGSP_BIC_EMZ";
+
   // Detect interactive mode (if no arguments) and define UI session
   //
-  G4UIExecutive* ui = nullptr;
-  if ( argc == 1 ) {
+  G4UIExecutive *ui = nullptr;
+  if (argc == 1)
+  {
     ui = new G4UIExecutive(argc, argv);
   }
 
+  // Register Geant4 scoring commands: /score/...
+  //
+  G4ScoringManager::GetScoringManager();
+
+
   // Construct the default run manager
   //
-  auto* runManager = G4RunManagerFactory::CreateRunManager();
+  auto *runManager = G4RunManagerFactory::CreateRunManager();
   G4int nThreads = 1;
   runManager->SetNumberOfThreads(nThreads);
 
-
   // Physics list factory
   G4PhysListFactory factory;
-  G4VModularPhysicsList* physList = nullptr;
+  G4VModularPhysicsList *physList = nullptr;
   //  runManager->SetUserInitialization(new FTFP_BERT);
 
-  //use cases - reference physics lists
-  //https://geant4.web.cern.ch/node/302
-  //EM physics lists
-  //https://geant4.web.cern.ch/node/146
-  physListName = "QGSP_BIC_EMZ";
+  // use cases - reference physics lists
+  // https://geant4.web.cern.ch/node/302
+  // EM physics lists
+  // https://geant4.web.cern.ch/node/146
+  //physListName = "QGSP_BIC_EMZ";
+  if (argc > 2) {
+  physListName = argv[2];
+  }
+
+
   // Check if the name is known to the factory
-  if ( physListName.size() &&  (! factory.IsReferencePhysList(physListName) ) ) {
+  if (physListName.size() && (!factory.IsReferencePhysList(physListName)))
+  {
     G4cerr << "Physics list " << physListName
            << " is not available in PhysListFactory." << G4endl;
     physListName.clear();
   }
-   // Reference PhysicsList via its name
+  // Reference PhysicsList via its name
   physList = factory.GetReferencePhysList(physListName);
 
   // Set mandatory initialization classes
   //
-  DetectorConstruction* detector = new DetectorConstruction;
+  DetectorConstruction *detector = new DetectorConstruction;
   runManager->SetUserInitialization(detector);
   runManager->SetUserInitialization(physList);
 
@@ -96,20 +112,22 @@ int main(int argc,char** argv)
 
   // Initialize visualization
   //
-  G4VisManager* visManager = new G4VisExecutive;
+  G4VisManager *visManager = new G4VisExecutive;
   visManager->Initialize();
 
   // Get the pointer to the User Interface manager
   //
-  G4UImanager* UImanager = G4UImanager::GetUIpointer();
+  G4UImanager *UImanager = G4UImanager::GetUIpointer();
 
-  if ( ! ui ) {
+  if (!ui)
+  {
     // batch mode
     G4String command = "/control/execute ";
     G4String fileName = argv[1];
-    UImanager->ApplyCommand(command+fileName);
+    UImanager->ApplyCommand(command + fileName);
   }
-  else {
+  else
+  {
     // interactive mode
     UImanager->ApplyCommand("/control/execute init_vis.mac");
     ui->SessionStart();
